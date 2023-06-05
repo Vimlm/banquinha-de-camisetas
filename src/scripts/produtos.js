e08e7f26b72c347b90fa134b06ea2bd72c02ba5c
Victor Martins <VictorMLMartins@icloud.com>
2023-06-05 18:07:52 -0300

    feat: comentarios proutos global cores fontes style produtos.js

--- src/scripts/produtos.js
+++ src/scripts/produtos.js
@@ -45,3 +45,5 @@ function comprar() {
     }
   }
 }
+
+// Aqui temos nossa interação JS com nossos produtos, a lógica funciona exatamente igual para as 3 opções, no caso temos a criação das função, uma variável controle "condicco" e abrimos um loop que pergunta para o usuário se ele realmente deseja adicionar ou comprar o item, o tratamento dessa informação é feito aceitando receber somente a opção "1" ou "2" que irá ou cancelar ou adicionar o produto dando um alert na tela e tornando nossa variável controle "condicao" = false interrompendo o loop, caso o usuário não coloque a opção que desejamos somente é enviado um "alert" na tela falando que a opção é inválida e voltando para o começo do loop que é "opcao".
